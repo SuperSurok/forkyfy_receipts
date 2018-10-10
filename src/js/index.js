66cdb8d6f0249563335b891fa1292566fad721b7
Sergey Lebedev <oddyone@mail.ru>
2018-10-10 23:05:17 +0300

    Building the Search Model.

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,6 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '78c197df622413cb3e8c820903faca99';
-    try {
-        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (err) {
-        console.log(err);
-    }
-}
+const search = new Search('pizza');
+console.log(search);
 
-getResults('pizza');
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '78c197df622413cb3e8c820903faca99';
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
