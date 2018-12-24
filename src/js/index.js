b9e00df62c4a6fbfd4a6320d55678a6637377c7a
Sergey Lebedev <oddyone@mail.ru>
2018-12-24 22:46:51 +0300

    Building the Likes Controller.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
 import List from "./models/List";
+import Likes from "./models/Likes";
 
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
@@ -122,6 +123,7 @@ const controlList = () => {
 };
 
 
+
 // Handle delete and update list item events
 elements.shopping.addEventListener('click', e => {
     const id = e.target.closest('.shopping__item').dataset.itemid;
@@ -142,6 +144,43 @@ elements.shopping.addEventListener('click', e => {
     }
 });
 
+
+
+/**
+ * Like Controller
+ **/
+const controlLIke = () => {
+  if(!state.likes) state.likes = new Likes();
+  const currentID = state.recipe.id;
+
+  // User has NOT yet liked current recipe
+    if(!state.likes.isLiked(currentID)) {
+        // Add like to the state
+        const newLike = state.likes.addLIke(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        );
+        // Toggle the like list
+
+        // Add like to UI list
+        console.log(state.likes);
+
+    // User HAS liked current recipe
+    } else {
+        // Remove like from the state
+        state.likes.deleteLike(currentID);
+
+        // Toggle the like button
+
+        // Remove like from UI list
+        console.log(state.likes);
+    }
+};
+
+
+
 // Handling recipe button clicks
 elements.recipe.addEventListener("click", e => {
     if (e.target.matches(".btn-decrease, .btn-decrease *")) {
@@ -155,7 +194,10 @@ elements.recipe.addEventListener("click", e => {
         state.recipe.updateServings("inc");
         recipeView.updateServingsIngredients(state.recipe);
     } else if (e.target.matches(".recipe__btn--add, .recipe__btn--add *")) {
+        // Add ingredients to shopping list
         controlList();
+    } else if (e.target.matches(".recipe__love, .recipe__love *")) {
+        controlLIke();
     }
 });
 
--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -1,4 +1,4 @@
-export default class LIkes {
+export default class Likes {
     constructor() {
         this.likes = [];
     }
