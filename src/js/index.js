338084fc639b05e8068c0c195b3510f99d6311bc
Sergey Lebedev <oddyone@mail.ru>
2019-01-03 16:22:26 +0300

    Implementing Persistent Data with localStorage.

--- src/js/index.js
+++ src/js/index.js
@@ -151,11 +151,6 @@ elements.shopping.addEventListener('click', e => {
 /**
  * Like Controller
  **/
-
-// For testing only. Will be remove in the next version app.
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-// ----------------------------
 const controlLIke = () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -187,6 +182,20 @@ const controlLIke = () => {
     }
     likesView.toggleLikeMenu(state.likes.getNumLikes())
 };
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 
 // Handling recipe button clicks
